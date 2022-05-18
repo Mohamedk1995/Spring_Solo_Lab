ec3df0a102dc825224c1257b8ad6f62e63fe4f48
bright-l-admin <bright-l-admin@BRIGHT-OS-14.cust.communityfibre.co.uk>
2022-05-18 15:40:16 +0100

    first commit

---
+++ src/main/java/com/example/bnta/author_book_lab/AuthorBookLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.bnta.author_book_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorBookLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorBookLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bnta/author_book_lab/AuthorBookLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bnta.author_book_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorBookLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
