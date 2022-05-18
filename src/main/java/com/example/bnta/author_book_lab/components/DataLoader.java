233b814c3d85b8fcbaf796c706f8b9bef6a4b78e
bright-l-admin <bright-l-admin@BRIGHT-OS-14.cust.communityfibre.co.uk>
2022-05-18 16:16:37 +0100

    Books and Authors added to DataLoader

--- src/main/java/com/example/bnta/author_book_lab/components/DataLoader.java
+++ src/main/java/com/example/bnta/author_book_lab/components/DataLoader.java
@@ -22,12 +22,19 @@ public class DataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Author author1 = new Author("Shakespeare");
-        authorRepository.save(author1);
+
+        //collected Authors together and books so that data can be added easily and keeps code clean.
+        Author author1 = new Author("William Shakespeare");
+        Author author2 = new Author("George Orwell");
+        authorRepository.saveAll(Arrays.asList(author1,author2));
 
         Book book1 = new Book("Hamlet", "Drama", author1);
         Book book2 = new Book("Macbeth", "Drama", author1);
         Book book3 = new Book("Othello", "Drama", author1);
-        bookRepository.saveAll(Arrays.asList(book1,book2,book3));
+        Book book4 = new Book("1984", "Dystopian", author2);
+        Book book5 = new Book("Animal Farm", "Political Satire", author2);
+        bookRepository.saveAll(Arrays.asList(book1,book2,book3,book4,book5));
+
+
     }
 }
