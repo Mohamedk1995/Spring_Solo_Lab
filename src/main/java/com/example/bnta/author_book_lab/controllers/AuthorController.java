ee8534c226e0b636346f87c42735259074a738da
bright-l-admin <bright-l-admin@BRIGHT-OS-14.cust.communityfibre.co.uk>
2022-05-18 16:25:27 +0100

    AuthorController updated with Index route

--- src/main/java/com/example/bnta/author_book_lab/controllers/AuthorController.java
+++ src/main/java/com/example/bnta/author_book_lab/controllers/AuthorController.java
@@ -1,4 +1,27 @@
 package com.example.bnta.author_book_lab.controllers;
 
+import com.example.bnta.author_book_lab.models.Author;
+import com.example.bnta.author_book_lab.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("authors") //localhost:8080/authors
 public class AuthorController {
+
+    @Autowired
+    AuthorRepository authorRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Author>> getAllAuthors() {
+        return new ResponseEntity<>(authorRepository.findAll(), HttpStatus.OK);
+    }
+
+
 }
