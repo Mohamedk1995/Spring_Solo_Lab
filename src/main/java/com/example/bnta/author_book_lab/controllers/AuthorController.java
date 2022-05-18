9dcc7794474d2044c15cbf17ed110d4b2661a443
bright-l-admin <bright-l-admin@BRIGHT-OS-14.cust.communityfibre.co.uk>
2022-05-18 16:56:44 +0100

    SHOW and POST routes added

--- src/main/java/com/example/bnta/author_book_lab/controllers/AuthorController.java
+++ src/main/java/com/example/bnta/author_book_lab/controllers/AuthorController.java
@@ -5,11 +5,10 @@ import com.example.bnta.author_book_lab.repositories.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("authors") //localhost:8080/authors
@@ -18,10 +17,26 @@ public class AuthorController {
     @Autowired
     AuthorRepository authorRepository;
 
+    //INDEX
     @GetMapping
     public ResponseEntity<List<Author>> getAllAuthors() {
         return new ResponseEntity<>(authorRepository.findAll(), HttpStatus.OK);
     }
+
+    //SHOW
+    @GetMapping(value="/{id}")//localhost:8080/authors/1
+    public ResponseEntity <Optional<Author>> getAuthor(@PathVariable Long id) {
+        return new ResponseEntity<>(authorRepository.findById(id),HttpStatus.OK);
+    }
+
+    //POST
+    @PostMapping
+    public ResponseEntity<Author> createAuthor(@RequestBody Author newAuthor) {
+        authorRepository.save(newAuthor);
+        return new ResponseEntity<>(newAuthor, HttpStatus.CREATED);
+    }
+
+
 
 
 }
