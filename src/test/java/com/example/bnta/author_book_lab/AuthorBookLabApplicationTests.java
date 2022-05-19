974223616d3f09b35c0e923a41cedcc6e6831583
bright-l-admin <bright-l-admin@BRIGHT-OS-14.cust.communityfibre.co.uk>
2022-05-19 15:03:10 +0100

    Testing added for derived queries

--- src/test/java/com/example/bnta/author_book_lab/AuthorBookLabApplicationTests.java
+++ src/test/java/com/example/bnta/author_book_lab/AuthorBookLabApplicationTests.java
@@ -1,18 +1,37 @@
 package com.example.bnta.author_book_lab;
 
+import com.example.bnta.author_book_lab.repositories.AuthorRepository;
 import com.example.bnta.author_book_lab.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 class AuthorBookLabApplicationTests {
 
-
-	@Test
-	void contextLoads() {
-	}
-
+//	@Autowired
+//	private BookRepository bookRepository;
+//
+//	@Autowired
+//	private AuthorRepository authorRepository;
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//	@Test
+//	public void testFindByName() {
+//		assertThat(authorRepository.findByName("George Orwell").size()).isEqualTo(1);
+//	}
+//
+//	@Test
+//	public void testFindByTitle() {
+//		assertThat(bookRepository.findByTitle("Hamlet").size()).isEqualTo(1);
+//}
+//
+//
 
 
 }
